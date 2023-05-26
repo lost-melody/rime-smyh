d0587cfe66cb0e024de6e35a140e4db6d635b013
lost-melody <lost__melody@163.com>
2023-05-26 21:10:19 +0800

    update embeded cands ui

--- template/lua/smyh/embeded_cands.lua
+++ template/lua/smyh/embeded_cands.lua
@@ -18,6 +18,12 @@ key_binder:
 
 local index_indicators = {"¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹", "⁰"}
 
+-- 首選/非首選格式定義
+-- Stash: 延迟候選; Seq: 候選序號; Code: 編碼; 候選: 候選文本; Comment: 候選提示
+local first_format = "Stash[候選Code]Comment"
+local next_format = "StashSeq候選Comment"
+local separator = ""
+
 -- 讀取 schema.yaml 開關設置:
 local option_name = "embeded_cands"
 local embeded_cands = nil
@@ -39,6 +45,61 @@ function embeded_cands_filter.init(env)
     env.engine.context.option_update_notifier:connect(handler)
 end
 
+-- 處理候選文本和延迟串
+local function render_stashcand(seq, stash, text, digested)
+    if string.len(stash) ~= 0 and string.match(text, "^"..stash) then
+        if seq == 1 then
+            -- 首選含延迟串, 原樣返回
+            digested = true
+            stash, text = stash, string.sub(text, string.len(stash)+1)
+        elseif not digested then
+            -- 首選不含延迟串, 其他候選含延迟串, 標記之
+            digested = true
+            stash, text = "["..stash.."]", string.sub(text, string.len(stash)+1)
+        else
+            -- 非首個候選, 延迟串標記爲空
+            stash, text = "", string.sub(text, string.len(stash)+1)
+        end
+    else
+        -- 普通候選, 延迟串標記爲空
+        stash, text = "", text
+    end
+    return stash, text, digested
+end
+
+-- 渲染提示, 因爲提示經常有可能爲空, 抽取爲函數更昜操作
+local function render_comment(comment)
+    if string.match(comment, "^~") then
+        -- 丟棄以"~"開頭的提示串, 這通常是補全提示
+        comment = ""
+    else
+        -- 自定義提示串格式
+        -- comment = "<"..comment..">"
+    end
+    return comment
+end
+
+-- 渲染單個候選項
+local function render_cand(seq, code, stashed, text, comment, digested)
+    local cand = ""
+    -- 選擇渲染格式
+    if seq == 1 then
+        cand = first_format
+    else
+        cand = next_format
+    end
+    -- 渲染延迟串與候選文字
+    stashed, text, digested = render_stashcand(seq, stashed, text, digested)
+    -- 渲染提示串
+    comment = render_comment(comment)
+    cand = string.gsub(cand, "Seq", index_indicators[seq])
+    cand = string.gsub(cand, "Code", code)
+    cand = string.gsub(cand, "Stash", stashed)
+    cand = string.gsub(cand, "候選", text)
+    cand = string.gsub(cand, "Comment", comment)
+    return cand, digested
+end
+
 -- 過濾器
 function embeded_cands_filter.func(input, env)
     if not embeded_cands then
@@ -51,15 +112,28 @@ function embeded_cands_filter.func(input, env)
     -- 要顯示的候選數量
     local page_size = env.engine.schema.page_size
     -- 暫存當前頁候選, 然后批次送出
-    local page_cands = {}
+    local page_cands, page_rendered = {}, {}
     -- 暫存索引, 首選和預編輯文本
     local index, first_cand, preedit = 0, nil, ""
+    local digested = false
+
+    local function refresh_preedit()
+        first_cand.preedit = table.concat(page_rendered, separator)
+        -- 將暫存的一頁候選批次送出
+        for _, c in ipairs(page_cands) do
+            yield(c)
+        end
+        -- 清空暫存
+        first_cand, preedit = nil, ""
+        page_cands, page_rendered = {}, {}
+        digested = false
+    end
 
     -- 迭代器
     local iter, obj = input:iter()
     -- 迭代由翻譯器輸入的候選列表
     local next = iter(obj)
-    local first_stash = true
+    -- local first_stash = true
     while next do
         -- 頁索引自增, 滿足 1 <= index <= page_size
         index = index + 1
@@ -80,48 +154,15 @@ function embeded_cands_filter.func(input, env)
         end
 
         -- 帶有暫存串的候選合併同類項
-        local cand_text = cand.text
-        local stash_len = string.len(core.stashed_text)
-        if string.len(core.stashed_text) ~= 0 and string.sub(cand_text, 1, stash_len) == core.stashed_text then
-            if index == 1 then
-                first_stash = false
-                -- "iwl"+"宇浩" => "宇[浩iwl]"
-                preedit = core.stashed_text.."["..string.sub(cand_text, stash_len+1)..input_code.."]"
-            elseif first_stash then
-                first_stash = false
-                -- "kjr"+"時間,峙沓" => "[時間kjr][峙]²沓"
-                preedit = preedit.."["..core.stashed_text.."]"..index_indicators[index]..string.sub(cand_text, stash_len+1)
-            else
-                -- "kjr"+"時間,峙沓,峙間" => "[時間kjr][峙]²沓³間"
-                preedit = preedit..index_indicators[index]..string.sub(cand_text, stash_len+1)
-            end
-        else
-            if index == 1 then
-                -- "kjr"+"時間" => "[時間kjr]"
-                preedit = "["..cand_text..input_code.."]"
-            elseif string.len(cand_text) > 0 then
-                -- "kjr"+"沓,間" => "[沓kjr]²間"
-                preedit = preedit..index_indicators[index]..cand_text
-            end
-        end
+        preedit, digested = render_cand(index, input_code, core.stashed_text, cand.text, cand.comment, digested)
 
-        -- 如果候選有提示且不以 "~" 開頭(补全提示), 識别爲反查提示
-        if string.len(cand.comment) ~= 0 and string.sub(cand.comment, 1, 1) ~= "~" then
-            preedit = preedit..cand.comment
-        end
-        -- 存入首選
+        -- 存入候選
         table.insert(page_cands, cand)
+        table.insert(page_rendered, preedit)
 
         -- 遍歷完一頁候選後, 刷新預編輯文本
         if index == page_size then
-            first_cand.preedit = preedit
-            -- 将暫存的一頁候選批次送出
-            for _, c in ipairs(page_cands) do
-                yield(c)
-            end
-            -- 清空暫存
-            first_cand, preedit = nil, ""
-            page_cands = {}
+            refresh_preedit()
         end
 
         -- 當前候選處理完畢, 查詢下一個
@@ -129,14 +170,7 @@ function embeded_cands_filter.func(input, env)
 
         -- 如果當前暫存候選不足page_size但没有更多候選, 則需要刷新預編輯並送出
         if not next and index < page_size then
-            first_cand.preedit = preedit
-            -- 将暫存的前三候選批次送出
-            for _, c in ipairs(page_cands) do
-                yield(c)
-            end
-            -- 清空暫存
-            first_cand, preedit = nil, ""
-            page_cands = {}
+            refresh_preedit()
         end
 
         -- 下一頁, index歸零
