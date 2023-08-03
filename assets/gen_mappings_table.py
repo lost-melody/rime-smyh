34185503be2b828704dde283d3a55621a15d28ce
lost-melody <lost__melody@163.com>
2023-08-03 12:10:54 +0800

    fix mappings table

--- assets/gen_mappings_table.py
+++ assets/gen_mappings_table.py
@@ -9,24 +9,24 @@ import sys
 # {"Q": ["食Qd", "户Qh", ...], "W": [...]}
 mappings: dict[str, list[str]] = {}
 
+space = " " * 7
+banner = "│"
+
 def get_row_data(key: str, line: int, cols: int) -> str:
     '''
     generate a row of data from mappings of key
     '''
     if line == 0:
         # head line
-        return "="*8 + " "*3 + key + " "*3 + "="*8 + "\t"
+        return "="*8 + " "*3 + key + " "*3 + "="*8
     offset, limit = (line-1)*cols, cols
     comps = mappings[key]
     if offset + limit <= len(comps):
-        # a\tb\tc\t
-        return "\t".join(comps[offset:offset+limit]) + "\t"
+        return banner.join(comps[offset:offset+limit])
     elif offset < len(comps):
-        # a\tb\t\t
-        return "\t".join(comps[offset:len(comps)]) + "\t"*(offset+limit+1-len(comps))
+        return banner.join(comps[offset:len(comps)] + [space]*(offset+limit-len(comps)))
     else:
-        # \t\t\t
-        return "\t"*limit
+        return banner.join([space] * limit)
 
 # read all mappings from stdin
 for line in sys.stdin.readlines():
@@ -38,15 +38,15 @@ for line in sys.stdin.readlines():
     # │食   Qd│戶   Qh│户   Qh│
     mappings[key].append(comp + " "*(5-len(comp)*2) + code)
 
-for row in ["QWERT", "YUIOP", "ASDFG", "HJKL", "XCVB", "NM"]:
+for row in ["QWERT", "YUIOP", "ASDFG", "HJKL", "ZXCVB", "NM"]:
     # suppose that every table has 10 rows, then filter the empty ones
     for i in range(0, 10):
         line = []
         for key in row:
+            if not mappings.get(key):
+                continue
             line.append(get_row_data(key, i, 3))
-        line = "\t".join(line).rstrip("\t")
-        if len(line) != 0:
-            print("\t", line, "\t", sep="")
+        line = (banner+space+banner).join(line).rstrip(" "+banner)
+        if len(line.strip(" "+banner)) != 0:
+            print(space+banner, line, banner, sep="")
     print()
-
-print("\tvim:\tts=8\tlcs=tab\\:\\ \\ \\│\t")
