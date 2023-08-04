354050d8c372fc3dce25b661b1861689516aa567
lost-melody <lost__melody@163.com>
2023-08-04 20:39:11 +0800

    update gen_dazhu.sh

--- assets/gen_dazhu.sh
+++ assets/gen_dazhu.sh
@@ -5,4 +5,8 @@
 # Usage:
 #   cat schema/smyh.base.dict.yaml | assets/gen_dazhu.sh >/tmp/wafel-dazhu.txt
 
-sed 's/\t/{TAB}/g' | grep '.*{TAB}.*{TAB}.*' | sed 's/\(.*\){TAB}\(.*\){TAB}.*/\2\t\1/g'
+sed 's/\t/{TAB}/g' | \
+    grep '.*{TAB}.*{TAB}.*' | \
+    sed 's/1/_/g' | \
+    sed 's/2/;/g' | \
+    sed 's/\(.*\){TAB}\(.*\){TAB}.*/\2\t\1/g'
