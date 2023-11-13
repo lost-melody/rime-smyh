832461022d7bba7a98cd9f75c81c42f1dfffe0a0
lost-melody <lost__melody@163.com>
2023-11-13 11:30:46 +0800

    fix mappings; update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-set -x
+# set -x
 cd "$(dirname $0)"
 WD="$(pwd)"
+NAME="${1:-wafel}"
+
+mkdir -p /tmp/"${NAME}"
+cp table/*.txt /tmp/"${NAME}"
+if [ -d deploy/"${NAME}" ]; then
+    cp -r deploy/"${NAME}"/*.txt /tmp/"${NAME}"
+fi
 
 # 创建目录
 SCHEMA="schema"
@@ -13,7 +20,13 @@ mkdir -p "${SCHEMA}/opencc"
 # 簡體碼表
 cd generator
 # 运行码表生成器
-go run . || exit 1
+go run . \
+    -d /tmp/"${NAME}"/smyh_div.txt \
+    -s /tmp/"${NAME}"/smyh_simp.txt \
+    -m /tmp/"${NAME}"/smyh_map.txt \
+    -f /tmp/"${NAME}"/freq.txt \
+    -w /tmp/"${NAME}"/cjkext_whitelist.txt \
+    || exit 1
 cd "${WD}"
 # 拷贝模板文件
 cp template/*.yaml template/*.txt "${SCHEMA}/"
@@ -21,24 +34,28 @@ cp template/lua/smyh/*.lua "${SCHEMA}/lua/smyh/"
 cp template/opencc/*.json "${SCHEMA}/opencc/"
 # 单字码表
 cat /tmp/char.txt >>"${SCHEMA}/smyh.base.dict.yaml"
-grep -v '#' table/smyh_quick.txt >>"${SCHEMA}/smyh.base.dict.yaml"
+grep -v '#' /tmp/"${NAME}"/smyh_quick.txt >>"${SCHEMA}/smyh.base.dict.yaml"
 # 单字四码码表
 cat /tmp/fullcode.txt >>"${SCHEMA}/smyh.yuhaofull.dict.yaml"
 # 拆分提示
 cat /tmp/div.txt >"${SCHEMA}/opencc/smyh_div.txt"
 # 生成字根表
-# cat table/smyh_map.txt | python assets/gen_mappings_table.py >assets/mappings_table.txt
+cat /tmp/"${NAME}"/smyh_map.txt | python assets/gen_mappings_table.py >"${SCHEMA}"/mappings_table.txt
 
 # 繁體碼表
 cd generator
 go run . \
-    -f "../table/freq_tc.txt" \
-    -s "../table/smyh_simp_tc.txt" \
+    -d /tmp/"${NAME}"/smyh_div.txt \
+    -s /tmp/"${NAME}"/smyh_simp_tc.txt \
+    -m /tmp/"${NAME}"/smyh_map.txt \
+    -f /tmp/"${NAME}"/freq_tc.txt \
+    -w /tmp/"${NAME}"/cjkext_whitelist.txt \
     || exit 1
 cd "${WD}"
 cat /tmp/char.txt >>"${SCHEMA}/smyh_tc.base.dict.yaml"
-grep -v '#' table/smyh_quick_tc.txt >>"${SCHEMA}/smyh_tc.base.dict.yaml"
+grep -v '#' /tmp/"${NAME}"/smyh_quick_tc.txt >>"${SCHEMA}/smyh_tc.base.dict.yaml"
 cat /tmp/fullcode.txt >>"${SCHEMA}/smyh_tc.yuhaofull.dict.yaml"
 
+rm -r /tmp/"${NAME}"
 # 清理生成文件
 rm /tmp/{char,fullcode,div}.txt
